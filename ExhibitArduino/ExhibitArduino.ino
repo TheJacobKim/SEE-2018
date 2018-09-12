112bda4de8c28dfa3a8b745bc203a8e30a8c9492
TheJacobKim <hyk053@ucsd.edu>
2018-09-12 01:08:16 -0700

    Added led strip logic

--- ExhibitArduino/ExhibitArduino.ino
+++ ExhibitArduino/ExhibitArduino.ino
@@ -1,9 +1,9 @@
 /*
- * Filename: ExhibitArduino.ino
- * Author: Jacob Kim
- * Description: The main file for Wave Exhibit from SEE-2018.
- * Date: Sept 11, 2018
- */
+   Filename: ExhibitArduino.ino
+   Author: Jacob Kim
+   Description: The main file for Wave Exhibit from SEE-2018.
+   Date: Sept 11, 2018
+*/
 
 #include <Metro.h>
 #include <wavTrigger.h>
@@ -19,8 +19,8 @@ CRGB leds[NUM_LEDS];
 #define MIN A1
 
 // Our WAV Trigger object
-wavTrigger wTrig;      
-           
+wavTrigger wTrig;
+
 // Pin nums
 int buttonPins[] = {2, 3, 4, 5, 6};
 int LEDpins[] = {7, 8, 9, 10, 11};
@@ -33,19 +33,19 @@ volatile byte LEDstates[] = {LOW, LOW, LOW, LOW, LOW};
 int buttonNum;
 
 /*
- * Function Name: setup()
- * Function Prototype: void setup();
- * Description: Default function required to initialize Arduino.
- *              1. Setup Serial connection (9600)
- *              2. Start WAV Trigger
- *              3. Set all the tracks to loop forever
- *              4. Setup button and LED pins
- *              5. Setup LED strip
- * Parameters: None
- * Side Effects: None
- * Error Conditions: None
- * Return Value: None
- */
+   Function Name: setup()
+   Function Prototype: void setup();
+   Description: Default function required to initialize Arduino.
+                1. Setup Serial connection (9600)
+                2. Start WAV Trigger
+                3. Set all the tracks to loop forever
+                4. Setup button and LED pins
+                5. Setup LED strip
+   Parameters: None
+   Side Effects: None
+   Error Conditions: None
+   Return Value: None
+*/
 void setup() {
   // Serial monitor
   Serial.begin(9600);
@@ -68,14 +68,14 @@ void setup() {
     wTrig.trackLoop(i, true);
     wTrig.trackPlayPoly(i);
   }
-  
+
   // Buttons and LEDs
   for (int i = 0; i < 5; i++) {
     pinMode(buttonPins[i], INPUT);
 
     // Attach interrupts() for the button pins' accuracy.
     attachInterrupt(digitalPinToInterrupt(buttonPins[i]), buttonPressed, CHANGE);
-    
+
     pinMode(LEDpins[i], OUTPUT);
   }
 
@@ -83,14 +83,14 @@ void setup() {
 }
 
 /*
- * Function Name: loop()
- * Function Prototype: void loop();
- * Description: The main driver of this program
- * Parameters: None
- * Side Effects: 
- * Error Conditions: None
- * Return Value: None
- */
+   Function Name: loop()
+   Function Prototype: void loop();
+   Description: The main driver of this program
+   Parameters: None
+   Side Effects:
+   Error Conditions: None
+   Return Value: None
+*/
 void loop() {
   // Which sounds to play
   int soundByte = checkPotentiometer(analogRead(MIN), analogRead(MAX));
@@ -100,7 +100,11 @@ void loop() {
 
   // Play sounds accordingly
   playSound(soundByte);
+
+  // Check result and turn led strip green or red
+  checkResult(soundByte);
 }
+
 
 
 
---
+++ ExhibitArduino/result.ino
@@ -0,0 +1,24 @@
+/*
+   Function Name: checkResult()
+   Function Prototype: void checkResult(int soundByte);
+   Description: Check if the user isolated a sound correctly
+   Parameters: soundByte - tells which sound is being played
+   Side Effects: LEDs arary status will change
+   Error Conditions: None
+   Return Value: None
+*/
+
+void checkResult(int soundByte) {
+  if (soundByte == buttonNum) {
+    for ( int i = 0; i < NUM_LEDS; i++ ) {
+      leds[i] = CRGB::Green;
+      FastLED.show();
+      delay(500);
+    }
+  } else {
+    for ( int i = 0; i < NUM_LEDS; i++ ) {
+      leds[i] = CRGB::Red;
+      FastLED.show();
+    }
+  }
+}
